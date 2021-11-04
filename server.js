911f677fc2a3326c68708fd89f12695dfedf7869
IanMcbull <ianmugenya@gmail.com>
2021-11-04 14:14:26 +0300

    server changes

--- server.js
+++ server.js
@@ -5,27 +5,21 @@ const path = require('path');
 const debug = require('debug')('server');
 const port = process.env.PORT || 4400;
 const morgan = require('morgan');
+const indexRouter = require('./routes/indexRouter');
 const servicesRouter = require('./routes/servicesRoutes');
 const locationRoute = require('./routes/locationRoutes');
 const contactRouter = require('./routes/contactRoute');
 const loginRouter = require('./routes/loginRoute');
+
 app.use(express.static(path.join(__dirname, '/styles')));
 app.set('view engine', 'ejs');
 // app.use(morgan('dev'));
+app.use(indexRouter);
 app.use('/services', servicesRouter);
 app.use('/location', locationRoute);
 app.use('/contact', contactRouter);
 app.use('/login',loginRouter)
-try {
-    //Fetch the homepage
-    app.get('/', (req, res) => {
-        res.render('index', {
-            title: 'Shaves & Trims'
-        });
-    })
-} catch (err) {
-    throw new Error(err);
-}
+
 
 
 //Listen for connections
